b1a420201f6ffb3a7059f09b716032fc648c4b72
carlsborg <ran.dugal@gmail.com>
2020-10-18 21:46:58 +0200

    fix setup.py for pypi distribution

--- setup.py
+++ setup.py
@@ -4,25 +4,31 @@
 import os
 import sys
 
-
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+import setuptools
 
 if sys.argv[-1] == 'publish':
-    os.system('python setup.py sdist upload')
+    os.system('python setup.py sdist bdist_wheel')
     sys.exit()
 
-setup(
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name='rcviz',
     version='0.1',
     description='Python call graph visualization for recursive functions',
     author='Ran',
     author_email='dugal@gmx.com',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url='https://github.com/carlsborg/rcviz',
     packages=['rcviz'],
     include_package_data=True,
     install_requires=['pygraphviz'],
-    zip_safe=False,
+    python_requires='>=3.6',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GPL2",
+        "Operating System :: OS Independent",
+    ]
 )
