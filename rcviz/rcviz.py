d95ecd7fd40528b8652185ce6b1f808ed817913c
Del Bao <del.bao@chime.com>
2021-10-28 16:31:56 -0700

    fix error: "name 'node_data' is not defined" for python 3.9

--- rcviz/rcviz.py
+++ rcviz/rcviz.py
@@ -9,6 +9,28 @@ import logging
 import copy
 from typing import Dict, List
 
+class node_data(object):
+
+    def __init__(self, _args=None, _kwargs=None, _fnname="", _ret=None, _childmethods=[]):
+        self.args = _args
+        self.kwargs = _kwargs
+        self.fn_name = _fnname
+        self.ret = _ret
+        self.child_methods = _childmethods  # [ (method, gcounter) ]
+
+        self.auxdata = {}  # user assigned track data
+
+    def __str__(self):
+        return "%s -> child_methods: %s" % (self.nodestr(), self.child_methods)
+
+    def nodestr(self):
+        return "%s = %s(%s)" % (self.ret, self.fn_name, self.argstr())
+
+    def argstr(self):
+        s_args = ",".join([str(arg) for arg in self.args])
+        s_kwargs = ",".join([(str(k), str(v))
+                             for (k, v) in self.kwargs.items()])
+        return "%s%s" % (s_args, s_kwargs)
 class callgraph(object):
     '''singleton class that stores global graph data
        draw graph using pygraphviz
@@ -109,28 +131,6 @@ class callgraph(object):
         print("callviz: rendered to %s" % filename)
 
 
-class node_data(object):
-
-    def __init__(self, _args=None, _kwargs=None, _fnname="", _ret=None, _childmethods=[]):
-        self.args = _args
-        self.kwargs = _kwargs
-        self.fn_name = _fnname
-        self.ret = _ret
-        self.child_methods = _childmethods  # [ (method, gcounter) ]
-
-        self.auxdata = {}  # user assigned track data
-
-    def __str__(self):
-        return "%s -> child_methods: %s" % (self.nodestr(), self.child_methods)
-
-    def nodestr(self):
-        return "%s = %s(%s)" % (self.ret, self.fn_name, self.argstr())
-
-    def argstr(self):
-        s_args = ",".join([str(arg) for arg in self.args])
-        s_kwargs = ",".join([(str(k), str(v))
-                             for (k, v) in self.kwargs.items()])
-        return "%s%s" % (s_args, s_kwargs)
 
 
 class viz(object):
